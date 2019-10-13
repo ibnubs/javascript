337a36f921befa7e9e495f2d09ab4986d28bd39c
ibnu <ibnubs@gmail.com>
2019-10-13 17:49:12 +0700

    tugas5 hingga 7

---
+++ tugas5.js
@@ -0,0 +1,5 @@
+var jumlah =0;
+for (var i = 0; i<=20; i++){
+    var jumlah = i * 3
+    console.log("perulangan i sebanyak " + jumlah)
+}
---
+++ tugas6.js
@@ -0,0 +1,8 @@
+var tampung =0;
+for (var i=0; i<=20; i++){
+    if (i % 2 == 0) {
+        console.log("ini bilangan genap");
+    } else {
+        console.log(i);
+    }
+}
