518ad52e5708b2b7ae19db6b4600a17ae19f9962
ibnubs <ibnubs@gmail.com>
2019-10-20 15:10:46 +0700

    tugas29 dan regex exercise

---
+++ regextest.js
@@ -0,0 +1,12 @@
+function panggilRegexp(value) {
+    //Tulis Code Jawaban Kamu Di Bawah ini
+    console.log(/Belajar/.test(value))
+
+ }
+ 
+panggilRegexp("Belajar html");
+panggilRegexp("Belajar javascript");
+panggilRegexp("Belajar css");
+panggilRegexp("Belajar php");
+
+
